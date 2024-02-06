d3a10c73c871d66262583b013baae05272ca7d84
Nacim Osman <nmosman2002@gmail.com>
2024-02-06 14:06:24 -0600

    Added feature of limited guesses with variable into program

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 number = 10
+limit = 10
 
 print("I'm thinking of a number...")
 guess = str(input("What number am I thinking of? "))
+print(f'{limit-1} guesses remaining')
 
-while guess != str(number):
+while guess != str(number) and limit != 0:
+    limit -= 1
     guess = str(input("Try again: "))
     if guess == 'q':
         print(f"Sorry! The number was {number}.")
+    print(f'{limit-1} guesses remaining')
 
 
 if guess == str(number):
